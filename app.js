02c16244c2a4e318a20037115f24eed1b7f93898
xFelipe <felipegdmito@gmail.com>
2019-07-30 00:35:46 -0300

    Parte 1 do tutorial foi terminada

---
+++ app.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const db = require('./db/db');
+const bodyParser = require('body-parser');
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  //console.log(db.todos);
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db,
+  })
+});
+
+app.get('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo) => {
+    if (todo.id === id){
+      return res.status(200).send({
+        success: 'true',
+        message: 'todo retrieved successfully',
+        todo,
+      });
+    }
+  });
+  return res.status(404).send({
+    success: 'false',
+    message: 'todo does not exist'
+  });
+});
+
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title){
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if(!req.body.description){
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  }
+  db.push(todo);
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    todo
+  })
+});
+
+app.put('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if (todo.id === id){
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if(!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required',
+    });
+  }
+  console.log(req.body.description);
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    updatedTodo,
+  });
+});
+
+app.delete('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10)
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: 'true',
+        message: 'Todo deleted successfuly'
+      });
+    }
+  });
+
+  return res.status(404).send({
+    success: 'false',
+    message: 'todo not found',
+  });
+
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
